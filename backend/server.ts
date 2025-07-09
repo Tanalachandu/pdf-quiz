168a008883627b7641d5d8f132d99677b4fc7dad
Tanalachandu <126540942+Tanalachandu@users.noreply.github.com>
2025-07-09 18:48:34 +0530

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -22,5 +22,6 @@ app.use("/api/generate", generateRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+ console.log(`Server running on port ${PORT}`);
+
 });
